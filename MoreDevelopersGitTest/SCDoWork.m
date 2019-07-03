9c1fe3aac4ea0385c1d21134190b20932f109882
SCChen <1657316151@qq.com>
2019-07-03 19:29:20 +0800

    add SCDoWork classs

---
+++ MoreDevelopersGitTest/SCDoWork.m
@@ -0,0 +1,28 @@
+//
+//  SCDoWork.m
+//  MoreDevelopersGitTest
+//
+//  Created by Chen Ling on 2019/7/3.
+//  Copyright © 2019 Chen Ling. All rights reserved.
+//
+
+#import "SCDoWork.h"
+
+@interface SCDoWork()
+
+@property (nonatomic, assign)BOOL canDoTheJob;
+
+@end
+
+@implementation SCDoWork
+
+-(instancetype)init {
+    if (self = [super init]) {
+        // Initial
+        
+    }
+    
+    return (self);
+}
+
+@end
